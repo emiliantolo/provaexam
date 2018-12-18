7bde5e30b462a9c2ba3095de8ce8e7a8eeab8119
emiliantolo <emiliantolo@gmail.com>
2018-12-18 16:37:18 +0100

    commit 1
    commit 2
    
    commit 3
    
    commit 1
    
    commit 2
    
    commit 3

--- sendArea.js
+++ sendArea.js
@@ -5,10 +5,10 @@ let sendArea = function(req, res){
     let areaval=getArea([parseInt(req.query.side1), parseInt(req.query.side2)])
 
     if(areaval==-1){
-	res.send(res.status(400).json({area: areaval}))
+	res.status(400).json({area: areaval})
     }
     else{
-	res.send(res.status(200).json({area: areaval}))
+	res.status(200).json({area: areaval})
     }	
     
 }
